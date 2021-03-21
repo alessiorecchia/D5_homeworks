c823c5d0fb2f5d79b15fbf3b8d4056c29a3bd2d9
alessiorecchia <80115631+alessiorecchia@users.noreply.github.com>
2021-03-21 23:17:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ print("Hello World")
 
 print("Hello Team mates!")
 
-#It is very difficult to find something ti commit
+#It is very difficult to find something to commit
 for i in range(4):
     print(i**i)
+    
+#Online edit: do not do it at home!
+print("Never hard edit online!")
