546cfca4276dcb900d086beb880d877e1fa7dd08
alessiorecchia <recchia.alessio@yahoo.com>
2021-03-21 23:18:07 +0100

    Raising an exception

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ print("Hello World")
 
 print("Hello Team mates!")
 
-#It is very difficult to find something ti commit
+#It is very difficult to find something to commit
 for i in range(4):
     print(i**i)
+
+#new edit do raise a conflict exception
+print("This is an exception")
